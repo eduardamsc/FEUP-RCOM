01178c2d2ff89167a81edd9cabac7dad9165a76c
nunocr <nunocr963@gmail.com>
2017-10-19 15:00:43 +0100

    appRead progress

--- TP2/appAPI.h
+++ TP2/appAPI.h
@@ -1,9 +1,19 @@
 #include "llAPI.h"
 
 int appRead(int port) {
+  char filename[];
+  char
+  llopen_read(port);
+
+
+
+  llclose_Receiver(port);
+
+  writeLocalFile(filename, fileBuffer);
+
   return 0;
 }
 
-int appWrite(int port) {
+int appWrite(int port, char filename[]) {
   return 0;
 }
