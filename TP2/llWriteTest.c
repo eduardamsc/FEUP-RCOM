c862652f1a939b08d27a36da7cba9b0144ee25ac
Joao611 <joaofvde@gmail.com>
2017-10-29 16:18:56 +0000

    Test code

--- TP2/llWriteTest.c
+++ TP2/llWriteTest.c
@@ -5,8 +5,10 @@
 
 int main() {
     char port[] = "/dev/ttyS0";
-    char msg[12] = {'a', 0x5, '0', 0, 5, 7};
+    int msgSize = 12;
+  //  char msg[12] = {FLAG, ESC, 'a', 0x5, '0', ESC, 0, FLAG, 5, ESC, FLAG, ESC};
+    char msg[12] = {ESC, 'a', 0x5, '0', 0, 5};
     int fd = llopen(port, TRANSMITTER);
-    int bytesWritten = llwrite(fd, msg, strlen(msg));
+    int bytesWritten = llwrite(fd, msg, msgSize);
     printf("bytesWritten = %d\n", bytesWritten);
 }
