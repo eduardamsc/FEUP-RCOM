19e159e731323e978896c7cdf4098cbd61362ef8
joao611 <joaofvde@gmail.com>
2017-12-07 00:20:46 +0000

    stuff before dux

---
+++ Project 2/FTP_application/Url.h
@@ -0,0 +1,6 @@
+struct Url {
+	char *username;
+	char *password;
+	char *host;
+	char *path;
+}
---
+++ Project 2/FTP_application/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include "Url.h"
+
+void printUsage(char progName[]) {
+	printf("USAGE: %s ftp://[<user>:<password>@]<host>/<url-path>", progName);
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printUsage(argv[0]);
+		exit(1);
+	}
+
+	struct Url url;
+	memset(&url, 0, sizeof(url));
+	parseUrl(&url, argv[1]);
+
+	FtpData ftp;
+	setupConnection(&url, &ftp);
+
+	downloadFile(&url, &ftp);
+
+	printResult;
+}
